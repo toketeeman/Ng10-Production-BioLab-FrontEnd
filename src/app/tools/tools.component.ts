1152b908054b5b0e5b765235a8a20a4507f4fbd0
Dwight Spencer <dspencer@absci.com>
2020-09-27 16:07:40 -0700

    added skeleton tools page

--- src/app/tools/tools.component.ts
+++ src/app/tools/tools.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from '@angular/forms';
 
 @Component({
   templateUrl: './tools.component.html',
   styleUrls: ['./tools.component.scss']
 })
 export class ToolsComponent implements OnInit {
+  sequenceForm: FormGroup;          // For entering sequence for translation/properties.
 
-  constructor() { }
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.sequenceForm = this.fb.group({
+      sequenceType: ['', Validators.required],
+      sequence: ['', Validators.required]
+    });
   }
 
+  onChangeSequenceType(): void {
+    return;
+  }
 }
